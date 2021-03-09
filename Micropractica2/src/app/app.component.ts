b2cf044d1549c91fabfed6edc9a99106e184085a
xAngelDVC <angeljuancris@gmail.com>
2021-03-09 19:18:46 +0100

    Añadido el boton "rebajar"

--- Micropractica2/src/app/app.component.ts
+++ Micropractica2/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { variable } from '@angular/compiler/src/output/output_ast';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Micropractica2';
+  public x: number = 0;
+  marcas = ["RENAULT", "RENAULT", "RENAULT", "TESLA"]
+  modelos = ["scenic", "ibiza", "megane", "model 3"]
+  anos = ["2007", "2003", "2007", "2007"]
+  fechaVenta = ["04-2018", "03-2018", "	03-2018", "	03-2018"]
+  precios = [5000, 1200, 3500, 4000];
+  pvps = [this.precios[0] * 1.21, this.precios[1] * 1.21, this.precios[2] * 1.21, this.precios[3] * 1.21];
+
+  
+
+  public rebajar(i: number) {
+    this.precios[i] = this.precios[i] * 0.9;
+    this.pvps[i] = this.pvps[i] * 0.9;;
+  }
 }
