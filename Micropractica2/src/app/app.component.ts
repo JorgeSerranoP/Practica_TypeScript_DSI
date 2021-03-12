9fa823bfec949ff3c9bc38ade228c4ada7f05e5e
JorgeSerranoP <100405987@alumnos.uc3m.es>
2021-03-12 12:21:55 +0100

    Funcionalidad con ngfor

--- Micropractica2/src/app/app.component.ts
+++ Micropractica2/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { variable } from '@angular/compiler/src/output/output_ast';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -13,39 +14,47 @@ export class AppComponent {
   public input:string = "";
 
   coche1:ICoche={
+    imagen: "Micropractica2\dist\Micropractica2\assets\images\scenic.jpg",
     marca: "RENAULT",
     modelo: "scenic",
     ano: 2007,
     fecha: "04-2018",
     precio: 5000,
-    PVP: 6050
+    PVP: 6050,
+    visible:true
   };
 
   coche2:ICoche={
+    imagen: "Micropractica2/images/megane.jpg",
     marca: "SEAT",
     modelo: "ibiza",
     ano: 2003,
     fecha: "03-2018",
     precio: 1200,
-    PVP: 1452
+    PVP: 1452,
+    visible:true
   };
 
   coche3:ICoche={
+    imagen: "./Micropractica2/images/megane.jpg",
     marca: "RENAULT",
     modelo: "megane",
     ano: 2007,
     fecha: "03-2018",
     precio: 3500,
-    PVP: 4235
+    PVP: 4235,
+    visible:true
   };
 
   coche4:ICoche={
+    imagen: "./Micropractica2/images/megane.jpg",
     marca: "TESLA",
     modelo: "model 3",
     ano: 2007,
     fecha: "03-2018",
     precio: 4000,
-    PVP: 4840
+    PVP: 4840,
+    visible:true
   };
 
   public rebajar(coche:ICoche) {
@@ -53,24 +62,32 @@ export class AppComponent {
     coche.PVP = coche.PVP * 0.9;
   };
 
-  public vendido(id:string) {
-    document.getElementById(id)?.remove();
+  public vendido(i:number) {
+    this.coches.splice(i,1);
+    console.log(this.coches);
   };
 
-  public filtro(){
-    this.input = ""
+  public filtro(coche:ICoche){
+      if(this.input.toUpperCase() == coche.marca){
+        return true;
+      } else{
+        coche.visible = false;
+    }
+    return false;
   }
 
   public coches:Array<ICoche>=[this.coche1, this.coche2, this.coche3, this.coche4];
 }
 
 export interface ICoche{
+  imagen: string;
   marca?: string;
   modelo: string;
   ano: number;
   fecha:string;
   precio: number;
   PVP: number;
+  visible: boolean;
 }
 
 export class CCoche{
